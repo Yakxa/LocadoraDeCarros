b83b9887be80dc479c9f4ee20c151aeee8cee426
yakxa <gui.lherme.brunetto@hotmail.com>
2024-10-03 01:46:11 -0300

    Initial commit, setting up DDD project structure

---
+++ TesteLocadoraDeCarros.Api/Controllers/CarroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteLocadoraDeCarros.Domain.Models;
+
+namespace TesteLocadoraDeCarros.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarroController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var carro = new Carro {
+                Id = id,
+                Marca = "Fiat",
+                Modelo = "Uno", 
+                Ano = 2005, 
+                Disponivel = true };
+            return Ok(carro);
+        }
+
+    }
+}
